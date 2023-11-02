7a0bd463b3efc38a24cc5287f490dee63b6a37fe
Damian Temimi <damian.temimi@gmail.com>
2023-11-02 21:48:45 +0100

    Poprawki do zadania (formatowanie kodu)

--- src/main/java/first/project/EvenNumbers.java
+++ src/main/java/first/project/EvenNumbers.java
@@ -4,11 +4,11 @@ public class EvenNumbers {
     public static void main(String[] args) {
 
       System.out.println(checkIfEven(30));
-    }
+}
 
-   static boolean checkIfEven(int number){
-     if(number%2 != 0 || number == 0){
-         return false;
-      } else return true;
-    }
+static boolean checkIfEven(int number){
+  if(number%2 != 0 || number == 0){
+    return false;
+    } else return true;
+  }
 }
