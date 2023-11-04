5b67989101970b9e4c57ec8c763536b3895e4479
Damian Temimi <damian.temimi@gmail.com>
2023-11-04 20:15:56 +0100

    Poprawki do zadania (formatowanie kodu)

--- src/test/java/first/project/EvenNumbersTest.java
+++ src/test/java/first/project/EvenNumbersTest.java
@@ -5,30 +5,30 @@ import org.junit.jupiter.api.Test;
 
 public class EvenNumbersTest {
 
-@Test
-    void shouldCheckIfEven(){
+    @Test
+    void shouldCheckIfEven() {
 //given
-    int number = 4;
-    boolean expected = true;
+        int number = 4;
+        boolean expected = true;
 
 //when
-    boolean result = EvenNumbers.checkIfEven(number);
+        boolean result = EvenNumbers.checkIfEven(number);
 
 //then
-    Assertions.assertEquals(expected, result);
+        Assertions.assertEquals(expected, result);
     }
 
 
-@Test
-    void shouldCheckIfNotEven(){
+    @Test
+    void shouldCheckIfNotEven() {
 //given
-    int number = 5;
-    boolean expected = false;
+        int number = 5;
+        boolean expected = false;
 
 //when
-    boolean result = EvenNumbers.checkIfEven(number);
+        boolean result = EvenNumbers.checkIfEven(number);
 
 //then
-    Assertions.assertEquals(expected, result);
+        Assertions.assertEquals(expected, result);
     }
 }
